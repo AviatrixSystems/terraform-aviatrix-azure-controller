5f6d924ae7993a48520bd1e214525a30433e9506
Dennizz <root@ipaddr.nl>
2021-11-12 10:49:03 +0100

    add resource group name output

--- aviatrix_controller_build/outputs.tf
+++ aviatrix_controller_build/outputs.tf
@@ -3,7 +3,8 @@ data "azurerm_public_ip" "aviatrix_controller_public_ip_address" {
   resource_group_name = azurerm_resource_group.aviatrix_controller_rg.name
 
   depends_on = [
-  azurerm_resource_group.aviatrix_controller_rg]
+    azurerm_resource_group.aviatrix_controller_rg
+  ]
 }
 output "aviatrix_controller_public_ip_address" {
   value = data.azurerm_public_ip.aviatrix_controller_public_ip_address.ip_address
@@ -15,4 +16,8 @@ output "aviatrix_controller_private_ip_address" {
 
 output "aviatrix_controller_vnet" {
   value = azurerm_virtual_network.aviatrix_controller_vnet
+}
+
+output "aviatrix_controller_rg_name" {
+  value = azurerm_resource_group.aviatrix_controller_rg.name
 }
