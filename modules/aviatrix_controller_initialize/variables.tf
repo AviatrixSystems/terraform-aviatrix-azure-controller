7abe2723bdc1c4c107360d3bc590dd3a72d7c2d4
Shuo Liu <sliu@aviatrix.com>
2023-06-17 06:37:26 -0700

    AVX-42737: hide password during init

--- modules/aviatrix_controller_initialize/variables.tf
+++ modules/aviatrix_controller_initialize/variables.tf
@@ -15,6 +15,7 @@ variable "avx_controller_admin_email" {
 
 variable "avx_controller_admin_password" {
   type        = string
+  sensitive   = true
   description = "aviatrix controller admin password"
 }
 
