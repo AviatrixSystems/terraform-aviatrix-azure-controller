e142ea4f379a82088f8a03ab95e4190602b794eb
Randolph Chung <rchung@aviatrix.com>
2024-09-13 08:30:40 -0700

    Update azurerm version constraint and config

--- main.tf
+++ main.tf
@@ -3,13 +3,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.8.0"
+      // https://github.com/hashicorp/terraform-provider-azurerm/issues/24444
+      version = "3.85.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
       version = "~> 2.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
 
 module "aviatrix_controller_azure" {
